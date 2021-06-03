dd4d94d6bb652957b33a05ec456baff3a9fd7d23
Neha Gadge <gadge.neha@gmail.com>
2021-06-03 15:03:17 -0700

    adding search box

--- Robofriends/src/index.js
+++ Robofriends/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import 'tachyons'
-import { robots } from './robots'
-import CardList from './CardList'
-ReactDOM.render(
-  <CardList robots={ robots }/>,
+
+ReactDOM.render( <App />,
   document.getElementById('root')
 );
 
